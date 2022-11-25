d9537f351e61facf9c827d029325442fc89d2d9e
hoan7902 <tranthihuong792002@gmail.com>
2022-11-26 00:57:56 +0700

    initialize

---
+++ backend/server.js
@@ -0,0 +1,28 @@
+const express = require('express')
+const dotenv = require("dotenv")
+const { chats } = require('./data/data')
+const connectDB = require('./config/db')
+const colors = require('colors')
+const userRoutes = require('./routes/userRoutes')
+const chatRoutes = require('./routes/chatRoutes')
+const { notFound, errorHandler } = require('./middleware/errorMiddleware')
+
+dotenv.config()
+connectDB()
+const app = express()
+
+app.use(express.json())
+
+app.get('/', (req, res) => {
+    res.send('API is Running Successfully')
+})
+
+app.use('/api/user', userRoutes)
+app.use('/api/chat', chatRoutes)
+// app.use('/api/message', messageRoutes)
+
+app.use(notFound)
+app.use(errorHandler)
+
+const PORT = process.env.PORT || 5000
+app.listen(5000, console.log(`Hello Server ${PORT}`.yellow.bold))
